68d35bef373f9b28fb24d90a5cb85bf2b9b1fd4d
Rodi <rodideath@gmail.com>
2016-11-07 16:08:03 +0100

    First Commit
    First Commit to test Unity with GitHub

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dice : MonoBehaviour
+{
+    public GameObject pivot;
+    public GameObject player;
+
+    private bool isMoving = false;
+    private float angleCount = 0;
+    private float previousAngle = 0;
+    public float speed = 0.01f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        player = this.gameObject;
+
+
+        Debug.Log(player.name);
+        Debug.Log(pivot.name);
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        player.transform.RotateAround(pivot.transform.position, Vector3.forward * speed * Time.deltaTime, -1);
+
+        //player.transform.Rotate(Vector3.forward * speed * Time.deltaTime, -1);
+    }
+
+    public void Move()
+    {
+        previousAngle = this.transform.rotation.eulerAngles.z;
+        isMoving = true;
+    }
+}
