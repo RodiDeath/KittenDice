c1116deac23b2c1fc2a1938006bda62cfa36d2ac
Rodi <rodideath@gmail.com>
2016-11-29 19:40:58 +0100

    Sistema Multiidioma Implementado
    Creada clase LanguageManager para gestionar las strings de los idiomas.
    Falta crear un diccionario o algo asi para facilitar el uso de los ids
    de los strings.

---
+++ Assets/Scripts/LanguageManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Xml;
+
+public class LanguageManager : MonoBehaviour
+{
+    [SerializeField]
+    string language;
+    private TextAsset textAsset;
+    private XmlDocument xmlDoc;
+    
+    void Awake()
+    {
+        LoadLanguageDocument(language);
+    }
+
+    public void LoadLanguageDocument(string lang)
+    {
+        textAsset = (TextAsset)Resources.Load("Languages/" + lang);
+
+
+        if (textAsset == null)
+        {
+            Debug.Log("Language doesn't exists");
+            lang = "English";
+            textAsset = (TextAsset)Resources.Load("Languages/" + lang);
+        }
+
+        language = lang;
+
+        xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(textAsset.text);
+
+        Debug.Log("Loaded " + lang);
+    }
+
+    public string GetString(string id)
+    {
+        XmlNodeList list = xmlDoc.GetElementsByTagName("string");
+
+        foreach (XmlNode node in list)
+        {
+            if (node.Attributes["name"].Value.Equals(id))
+            {
+                return node.InnerText;
+            }
+        }
+        return "";
+    }
+}
