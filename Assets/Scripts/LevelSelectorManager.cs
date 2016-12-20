0dfa854a4a0d87f97d6ca4135dd10c4578eb0723
Rodi <rodideath@gmail.com>
2016-12-20 12:00:36 +0100

    Level Selector Showing Stars and Score

--- Assets/Scripts/LevelSelectorManager.cs
+++ Assets/Scripts/LevelSelectorManager.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using System.Text.RegularExpressions;
@@ -12,20 +12,47 @@ public class LevelSelectorManager : MonoBehaviour
 
     [SerializeField]
     RectTransform panelLevel;
+    
+    GameObject panelStars;
 
     private Vector3 levelScale;
 
     public static int world = 1;
     int levelsPerRow = 3;
     int levelsShowed = 0;
-    
+    string[,] levelDataTable;
+
     //private ScrolView
 
 
     // Use this for initialization
     void Start ()
     {
+        GetLevelsData();
         PopulateLevelSelector(world);
+        
+    }
+
+    public void GetLevelsData()
+    {
+        string levelDataPath = "Levels/LevelsData"; // Path of the txt level data file
+        TextAsset levelData = (TextAsset)Resources.Load(levelDataPath, typeof(TextAsset)); // Stores the txt file in a TextAsset variable
+
+        string[] splitFile = new string[] { "\r\n", "\r", "\n" }; // Set the split parameter (\r\n -> New Line)
+        string[] levelDataLines = levelData.text.Split(splitFile, System.StringSplitOptions.None); // Stores in a string[] all the txt lines separately (the séparation)
+
+        levelDataTable = new string[levelDataLines.Length, levelDataLines[0].Split('*').Length];
+
+        for (int i = 0; i < levelDataLines.Length; i++)
+        {
+            for (int j = 0; j < levelDataLines[0].Split('*').Length; j++)
+            {
+                levelDataTable[i,j] = levelDataLines[i].Split('*')[j];
+                //Debug.Log(levelDataTable[i,j]);
+            }
+        }
+
+
     }
 	
     void PopulateLevelSelector(int world)
@@ -39,11 +66,11 @@ public class LevelSelectorManager : MonoBehaviour
         List<string> levels = LevelManager.GetAllLevelStrings(world);
 
         // UI TEST ONLY /////////////////
-        for (int i = 0; i < 21; i++)
-        {
-            int levelN = i + 4;
-            levels.Add("Level" + levelN);
-        }
+        //for (int i = 0; i < 21; i++)
+        //{
+        //    int levelN = i + 4;
+        //    levels.Add("Level" + levelN);
+        //}
         ////////////////////////////////
 
 
@@ -53,14 +80,7 @@ public class LevelSelectorManager : MonoBehaviour
             if (!modelPanelDeleted)
             {
                 panelLevel = contentScrollView.GetChild(contentScrollView.childCount - 1).GetComponent<RectTransform>();
-                contentScrollView.GetComponent<RectTransform>().position = new Vector3(0,0,0);
-
-                
-
-                //RectTransform newPanelLevel = Instantiate(panelLevel, new Vector3(panelLevel.position.x, panelLevel.position.y, 1f), Quaternion.identity) as RectTransform;
-
-                //startPositionX = newPanelLevel.position.x;
-                //startPositionY = newPanelLevel.position.y;
+                contentScrollView.GetComponent<RectTransform>().position = new Vector3(0, 0, 0);
 
                 initialX = -280f;
                 initialY = -150f;
@@ -68,24 +88,26 @@ public class LevelSelectorManager : MonoBehaviour
                 startPositionX = initialX;
                 startPositionY = initialY;
 
-                //RectTransform newPanelLevel = Instantiate(panelLevel, new Vector3(panelLevel.position.x, panelLevel.position.y, 1f), Quaternion.identity) as RectTransform;
                 RectTransform newPanelLevel = Instantiate(panelLevel, new Vector3(startPositionX, startPositionY, 1f), Quaternion.identity) as RectTransform;
-
-                Debug.Log("StartX: " + startPositionX);
-                //RectTransform newPanelLevel = Instantiate(panelLevel, new Vector3(startPositionX, startPositionY, 1f), Quaternion.identity) as RectTransform;
-
-               // newPanelLevel.localPosition = new Vector3(startPositionX, startPositionY);
 
                 newPanelLevel.name = Regex.Match(level, @"\d+").Value;
                 newPanelLevel.GetComponentInChildren<Text>().text = newPanelLevel.name;
-                //newPanelLevel.localScale = new Vector3(1,1,1);
-                //levelScale = newPanelLevel.localScale;
 
                 newPanelLevel.SetParent(contentScrollView, false);
                 Destroy(contentScrollView.GetChild(0).gameObject);
                 modelPanelDeleted = true;
 
                 startPositionX += newPanelLevel.rect.width + 30;
+
+                /*************/
+                /* LevelData */
+                /*************/
+                // Stars
+                SetStars(newPanelLevel);
+                // Score
+                int levelIndex = Convert.ToInt32(newPanelLevel.name);
+                Transform paneScore = newPanelLevel.transform.GetChild(2);
+                paneScore.GetComponent<Text>().text = levelDataTable[levelIndex - 1, 3];
 
 
                 levelsShowed++;
@@ -94,20 +116,30 @@ public class LevelSelectorManager : MonoBehaviour
             {
                 panelLevel = contentScrollView.GetChild(contentScrollView.childCount - 1).GetComponent<RectTransform>();
 
-                //RectTransform newPanelLevel = Instantiate(panelLevel, new Vector3(panelLevel.position.x, panelLevel.position.y - panelLevel.rect.height, 1f), Quaternion.identity) as RectTransform;
                 RectTransform newPanelLevel = Instantiate(panelLevel, new Vector3(startPositionX, startPositionY, 1f), Quaternion.identity) as RectTransform;
 
                 newPanelLevel.localPosition = new Vector3(startPositionX, startPositionY);
 
                 newPanelLevel.name = Regex.Match(level, @"\d+").Value;
                 newPanelLevel.GetComponentInChildren<Text>().text = newPanelLevel.name;
-                //newPanelLevel.localScale = levelScale;
 
                 newPanelLevel.SetParent(contentScrollView, false);
-
-
-                Debug.Log("StartX: " + startPositionX);
+                
                 startPositionX += newPanelLevel.rect.width+30;
+
+
+
+                /*************/
+                /* LevelData */
+                /*************/
+                // Stars
+                SetStars(newPanelLevel);
+                // Score
+                int levelIndex = Convert.ToInt32(newPanelLevel.name);
+                Transform paneScore = newPanelLevel.transform.GetChild(2);
+                paneScore.GetComponent<Text>().text = levelDataTable[levelIndex - 1, 3];
+
+
                 levelsShowed++;
             }
 
@@ -122,6 +154,87 @@ public class LevelSelectorManager : MonoBehaviour
 
         contentScrollView.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical , (panelLevel.rect.height * levels.Count / levelsPerRow) + 250);
         contentScrollView.position = new Vector3(0,0,0);
+    }
+
+    private void SetStars(RectTransform newPanelLevel)
+    {
+        Transform panelStars = newPanelLevel.transform.GetChild(0);
+
+        int levelIndex = Convert.ToInt32(newPanelLevel.name);
+
+
+        int c = 0;
+        switch (levelDataTable[levelIndex - 1, 2])
+        {
+            case "0":
+                c = 0;
+                foreach (Transform star in panelStars)
+                {
+                    if (star.name.Contains("Full"))
+                    {
+                        if (c == 0) star.gameObject.SetActive(false);
+                        if (c == 1) star.gameObject.SetActive(false);
+                        if (c == 2) star.gameObject.SetActive(false);
+
+                        c++;
+                    }
+                }
+
+
+                break;
+
+
+            case "1":
+                c = 0;
+                foreach (Transform star in panelStars)
+                {
+                    if (star.name.Contains("Full"))
+                    {
+                        if (c == 0) star.gameObject.SetActive(true);
+                        if (c == 1) star.gameObject.SetActive(false);
+                        if (c == 2) star.gameObject.SetActive(false);
+                        c++;
+                    }
+
+                }
+
+
+                break;
+            case "2":
+                c = 0;
+                foreach (Transform star in panelStars)
+                {
+                    if (star.name.Contains("Full"))
+                    {
+                        if (c == 0) star.gameObject.SetActive(true);
+                        if (c == 1) star.gameObject.SetActive(true);
+                        if (c == 2) star.gameObject.SetActive(false);
+                        c++;
+                    }
+
+                }
+                break;
+
+            case "3":
+                c = 0;
+                foreach (Transform star in panelStars)
+                {
+                    if (star.name.Contains("Full"))
+                    {
+                        if (c == 0) star.gameObject.SetActive(true);
+                        if (c == 1) star.gameObject.SetActive(true);
+                        if (c == 2) star.gameObject.SetActive(true);
+                        c++;
+                    }
+
+                }
+
+                break;
+
+            default:
+                break;
+
+        }
     }
 
     public void Back()
